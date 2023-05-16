95e9f28db600d809d55cf40feb1dae251fa4a4ca
MDS9703 <mitchellstanley3@gmail.com>
2023-05-16 16:46:12 -0400

    user db

--- server/db/users.js
+++ server/db/users.js
@@ -1,7 +1,7 @@
 const client = require('./client');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const SALT_COUNT = 10;
 
 async function createUser({ username, password, email, isAdmin = false }) {
   const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
@@ -45,11 +45,41 @@ async function getUser({ username, password }) {
   }
 }
 
-async function getUserById() {
-  
+async function getUserById(userId) {
+  try {
+    const {rows: [user]} = await client.query(`
+    SELECT id, username, email, "isAdmin"
+    FROM users
+    WHERE id = $1;
+    `, [userId]);
+
+    if (!user) return null;
+    return user;  
+  } catch (error) {
+      throw error;
+  }
 }
 
-async function getUserByUsername() {}
+async function getUserByUsername(userName) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+    SELECT id, username, password, email
+    FROM users
+    WHERE username = $1;
+    `,
+      [userName]
+    );
+
+    if (!user) return null;
+
+    return user;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 module.exports = {
   createUser,

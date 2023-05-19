bc573424b5bdfeffb63db1b47929e3047fb718b3
cfrost <cassandra.e.hutchings@gmail.com>
2023-05-19 11:59:05 -0600

    categories

---
+++ client/src/api/data.js
@@ -0,0 +1,322 @@
+export const productsToMap = [
+    {
+        id: 1,
+        name: "ElegentErgo",
+        description: "Wooden grey chair.",
+        price: 227.24,
+        picture: "/img/chair1.webp",
+        category: "chair"
+    },
+    {
+        id: 2,
+        name: "CozyCushion",
+        description: "Plush fabric armchair with extra cushioning.",
+        price: 349.99,
+        picture: "/img/chair2.jpg",
+        category: "chair"
+    },
+    {
+        id: 3,
+        name: "SleekLeather",
+        description: "Black leather recliner chair with adjustable headrest.",
+        price: 599.99,
+        picture: "/img/chair3.webp",
+        category: "chair"
+    },
+    {
+        id: 4,
+        name: "Mid-CenturyMarvel",
+        description: "Iconic mid-century modern chair in vibrant colors.",
+        price: 299.99,
+        picture: "/img/chair4.webp",
+        category: "chair"
+    },
+    {
+        id: 5,
+        name: "RusticRocking",
+        description: "Handcrafted wooden rocking chair with a rustic finish.",
+        price: 179.99,
+        picture: "/img/chair5.webp",
+        category: "chair"
+    },
+    {
+        id: 6,
+        name: "ContemporaryChic",
+        description: "Sleek and stylish chair with metal frame and minimalist design.",
+        price: 249.99,
+        picture: "/img/chair6.jpg",
+        category: "chair"
+    },
+    {
+        id: 7,
+        name: "BohemianBreeze",
+        description: "Bohemian-inspired woven rattan chair with a comfortable cushion.",
+        price: 199.99,
+        picture: "/img/chair7.jpg",
+        category: "chair"
+    },
+    {
+        id: 8,
+        name: "VersatileVanity",
+        description: "Elegant vanity chair with a padded seat and mirrored accents.",
+        price: 129.99,
+        picture: "/img/chair8.webp",
+        category: "chair"
+    },
+    {
+        id: 9,
+        name: "IndustrialIron",
+        description: "Sturdy industrial-style chair with metal frame and distressed finish.",
+        price: 179.99,
+        picture: "/img/chair9.webp",
+        category: "chair"
+    },
+    {
+        id: 10,
+        name: "LuxeLounge",
+        description: "Luxurious velvet lounge chair with a tufted backrest and gold accents.",
+        price: 499.99,
+        picture: "/img/chair10.webp",
+        category: "chair"
+    },
+    {
+        id: 11,
+        name: "Grey Bliss",
+        description: "Grey sectional Couch",
+        price: 294.94,
+        picture: "/img/couch10.webp",
+        category: "couch"
+    },
+    {
+        id: 12,
+        name: "Grey Bliss",
+        description: "Grey sectional Couch",
+        price: 294.94,
+        picture: "/img/couch1.webp",
+        category: "couch"
+    },
+    {
+        id: 13,
+        name: "Modern Minimalist",
+        description: "Sleek and minimalist white leather couch with clean lines.",
+        price: 599.99,
+        picture: "/img/couch2.webp",
+        category: "couch"
+    },
+    {
+        id: 14,
+        name: "Vintage Velvet",
+        description: "Elegant burgundy velvet couch with tufted details and wooden legs.",
+        price: 799.99,
+        picture: "/img/couch3.webp",
+        category: "couch"
+    },
+    {
+        id: 15,
+        name: "Coastal Retreat",
+        description: "Beige linen couch with a relaxed coastal vibe and comfortable cushions.",
+        price: 499.99,
+        picture: "/img/couch4.webp",
+        category: "couch"
+    },
+    {
+        id: 16,
+        name: "Midnight Blue",
+        description: "Deep blue velvet couch with a luxurious feel and button-tufted backrest.",
+        price: 899.99,
+        picture: "/img/couch5.webp",
+        category: "couch"
+    },
+    {
+        id: 17,
+        name: "Convertible Charmer",
+        description: "Multifunctional gray couch that easily transforms into a comfortable sleeper sofa.",
+        price: 699.99,
+        picture: "/img/couch6.webp",
+        category: "couch"
+    },
+    {
+        id: 18,
+        name: "Rustic Retreat",
+        description: "Distressed leather couch with a weathered look and cozy oversized cushions.",
+        price: 1299.99,
+        picture: "/img/couch7.jpg",
+        category: "couch"
+    },
+    {
+        id: 19,
+        name: "Chic Chesterfield",
+        description: "Classic chesterfield-style couch in light gray with elegant button-tufting.",
+        price: 899.99,
+        picture: "/img/couch8.jpg",
+        category: "couch"
+    },
+    {
+        id: 20,
+        name: "Contemporary Comfort",
+        description: "Modern gray couch with adjustable headrests and built-in cup holders.",
+        price: 799.99,
+        picture: "/img/couch9.webp",
+        category: "couch"
+    },
+    {
+        id: 21,
+        name: "Standard Lamp",
+        description: "Really Really Bright",
+        price: 50.99,
+        picture: "/img/light1.jpg",
+        category: "light"
+    },
+    {
+        id: 22,
+        name: "Elegant Chandelier",
+        description: "Crystal chandelier with intricate design and sparkling lights.",
+        price: 299.99,
+        picture: "/img/light2.jpg",
+        category: "light"
+    },
+    {
+        id: 23,
+        name: "Industrial Floor Lamp",
+        description: "Tall floor lamp with exposed bulbs and metal construction for an industrial look.",
+        price: 129.99,
+        picture: "/img/light3.jpg",
+        category: "light"
+    },
+    {
+        id: 24,
+        name: "Modern Table Lamp",
+        description: "Minimalist table lamp with a sleek design and touch-sensitive controls.",
+        price: 79.99,
+        picture: "/img/light4.jpg",
+        category: "light"
+    },
+    {
+        id: 25,
+        name: "Vintage Desk Lamp",
+        description: "Antique-inspired desk lamp with a brass finish and adjustable arm.",
+        price: 89.99,
+        picture: "/img/light5.jpg",
+        category: "light"
+    },
+    {
+        id: 26,
+        name: "Tiffany Style Lamp",
+        description: "Stained glass lamp with vibrant colors and intricate patterns.",
+        price: 199.99,
+        picture: "/img/light6.jpg",
+        category: "light"
+    },
+    {
+        id: 27,
+        name: "Modern Pendant Light",
+        description: "Sleek and contemporary pendant light with a matte black finish.",
+        price: 149.99,
+        picture: "/img/light7.jpg",
+        category: "light"
+    },
+    {
+        id: 28,
+        name: "Rustic Lantern",
+        description: "Rustic lantern-style lamp with a wooden frame and soft ambient lighting.",
+        price: 79.99,
+        picture: "/img/light8.jpg",
+        category: "light"
+    },
+    {
+        id: 29,
+        name: "Glamorous Sconce",
+        description: "Glamorous wall sconce with crystal accents and a shimmering gold finish.",
+        price: 129.99,
+        picture: "/img/light9.jpg",
+        category: "light"
+    },
+    {
+        id: 30,
+        name: "Artistic Floor Lamp",
+        description: "Unique floor lamp with a sculptural design and artistic flair.",
+        price: 199.99,
+        picture: "/img/light10.jpg",
+        category: "light"
+    },
+    {
+        id: 31,
+        name: "Sleek Bedside Table",
+        description: "Sleek bedside table with a contemporary design and convenient storage drawers.",
+        price: 99.99,
+        picture: "/img/table1.jpg",
+        category: "table"
+    },
+    {
+        id: 32,
+        name: "Elegant Dining Table",
+        description: "Large dining table with a beautiful wooden finish and intricate carvings.",
+        price: 899.99,
+        picture: "/img/table2.jpg",
+        category: "table"
+    },
+    {
+        id: 33,
+        name: "Modern Glass Coffee Table",
+        description: "Sleek coffee table with a glass top and minimalist metal frame.",
+        price: 249.99,
+        picture: "/img/table3.jpg",
+        category: "table"
+    },
+    {
+        id: 34,
+        name: "Vintage Console Table",
+        description: "Antique-inspired console table with ornate details and a distressed finish.",
+        price: 349.99,
+        picture: "/img/table4.jpg",
+        category: "table"
+    },
+    {
+        id: 35,
+        name: "Industrial-Style Desk",
+        description: "Sturdy desk with a rustic industrial look and ample storage space.",
+        price: 499.99,
+        picture: "/img/table5.jpg",
+        category: "table"
+    },
+    {
+        id: 36,
+        name: "Contemporary Side Table",
+        description: "Modern side table with a unique geometric design and a marble top.",
+        price: 179.99,
+        picture: "/img/table6.jpg",
+        category: "table"
+    },
+    {
+        id: 37,
+        name: "Farmhouse Dining Table",
+        description: "Charming farmhouse-style dining table with a reclaimed wood look.",
+        price: 699.99,
+        picture: "/img/table7.webp",
+        category: "table"
+    },
+    {
+        id: 38,
+        name: "Glamorous Vanity Table",
+        description: "Glamorous vanity table with a mirrored surface and elegant design.",
+        price: 349.99,
+        picture: "/img/table8.webp",
+        category: "table"
+    },
+    {
+        id: 39,
+        name: "Minimalist Writing Desk",
+        description: "Minimalist writing desk with clean lines and a spacious work area.",
+        price: 299.99,
+        picture: "/img/table9.webp",
+        category: "table"
+    },
+    {
+        id: 40,
+        name: "Versatile Folding Table",
+        description: "Versatile folding table that can be easily stored and set up for various purposes.",
+        price: 129.99,
+        picture: "/img/table10.jpg",
+        category: "table"
+    },
+]
--- client/src/components/Routes.jsx
+++ client/src/components/Routes.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import AllProducts from './AllProducts';
 import Home from './Home';
 import Login from './Login';
+import Cart from './Cart';
 
 export const RRoutes = ({token, setToken, user, setUser}) => {
     return (
@@ -11,6 +12,7 @@ export const RRoutes = ({token, setToken, user, setUser}) => {
                 <Route path= "/" element= {<Home token={token} setToken={setToken} setUser={setUser} user={user}/>}></Route>
                 <Route path="/products" element={<AllProducts token={token} setToken={setToken} setUser={setUser} user={user}/>}> </Route>
                 <Route path="/login" element={<Login token={token} setToken={setToken} setUser={setUser} user={user}/>}></Route>
+                <Route path="/cart" element={<Cart token={token} setToken={setToken} setUser={setUser} user={user}/>}></Route>
             </Routes>
         </div>
     );
